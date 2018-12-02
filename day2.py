06152d60572558a8b46e81e955fe3d479d6248b8
Recursing <buonanno.lorenzo@gmail.com>
2018-12-02 16:26:45 +0100

    added one liner to make code even uglier

--- day2.py
+++ day2.py
@@ -7,3 +7,6 @@ print(sum(3 in set(Counter(l).values()) for l in lines) *
 removed = [[l[:i]+l[i+1:] for l in lines] for i in range(26)]
 counts = (Counter(r).most_common(1)[0] for r in removed)
 print(next(line for line,count in counts if count == 2))
+
+#oneliner for fun
+print(next(line for line,count in (Counter([l[:i]+l[i+1:] for l in lines]).most_common(1)[0] for i in range(26)) if count == 2))
